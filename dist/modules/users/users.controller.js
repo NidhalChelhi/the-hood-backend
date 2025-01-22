f59fcc90a4f92bbbb21b93b3f797223ee84d879c
Hasnaoui-Youssef <hasneouiyoussef@gmail.com>
2025-01-22 17:39:01 +0100

    Removed role guard from users

--- dist/modules/users/users.controller.js
+++ dist/modules/users/users.controller.js
@@ -17,8 +17,8 @@ const common_1 = require("@nestjs/common");
 const users_service_1 = require("./users.service");
 const create_user_dto_1 = require("./dto/create-user.dto");
 const update_user_dto_1 = require("./dto/update-user.dto");
-const roles_decorator_1 = require("../../common/decorators/roles.decorator");
 const user_query_dto_1 = require("./dto/user-query.dto");
+const public_decorator_1 = require("../../common/decorators/public.decorator");
 let UsersController = class UsersController {
     constructor(usersService) {
         this.usersService = usersService;
@@ -46,7 +46,6 @@ let UsersController = class UsersController {
 };
 exports.UsersController = UsersController;
 __decorate([
-    (0, roles_decorator_1.Roles)("admin"),
     (0, common_1.Post)(),
     __param(0, (0, common_1.Body)()),
     __metadata("design:type", Function),
@@ -54,7 +53,6 @@ __decorate([
     __metadata("design:returntype", Promise)
 ], UsersController.prototype, "create", null);
 __decorate([
-    (0, roles_decorator_1.Roles)("admin"),
     (0, common_1.Get)(),
     __param(0, (0, common_1.Query)()),
     __metadata("design:type", Function),
@@ -62,7 +60,6 @@ __decorate([
     __metadata("design:returntype", Promise)
 ], UsersController.prototype, "findAll", null);
 __decorate([
-    (0, roles_decorator_1.Roles)("admin"),
     (0, common_1.Get)(":id"),
     __param(0, (0, common_1.Param)("id")),
     __metadata("design:type", Function),
@@ -70,7 +67,6 @@ __decorate([
     __metadata("design:returntype", Promise)
 ], UsersController.prototype, "findOne", null);
 __decorate([
-    (0, roles_decorator_1.Roles)("admin"),
     (0, common_1.Patch)(":id"),
     __param(0, (0, common_1.Param)("id")),
     __param(1, (0, common_1.Body)()),
@@ -79,7 +75,6 @@ __decorate([
     __metadata("design:returntype", Promise)
 ], UsersController.prototype, "update", null);
 __decorate([
-    (0, roles_decorator_1.Roles)("admin"),
     (0, common_1.Delete)(":id"),
     __param(0, (0, common_1.Param)("id")),
     __metadata("design:type", Function),
@@ -87,6 +82,7 @@ __decorate([
     __metadata("design:returntype", Promise)
 ], UsersController.prototype, "delete", null);
 exports.UsersController = UsersController = __decorate([
+    (0, public_decorator_1.Public)(),
     (0, common_1.Controller)("users"),
     __metadata("design:paramtypes", [users_service_1.UsersService])
 ], UsersController);
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -12,26 +12,24 @@ import {
 import { UsersService } from "./users.service";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { UpdateUserDTO } from "./dto/update-user.dto";
-import { Roles } from "../../common/decorators/roles.decorator";
 import { UserQueryDTO } from "./dto/user-query.dto";
+import { Public } from "src/common/decorators/public.decorator";
 
+@Public()
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Roles("admin")
   @Post()
   async create(@Body() createUserDTO: CreateUserDTO) {
     return this.usersService.createUser(createUserDTO);
   }
 
-  @Roles("admin")
   @Get()
   async findAll(@Query() serachQuery : UserQueryDTO) {
     return this.usersService.findAll(serachQuery);
   }
 
-  @Roles("admin")
   @Get(":id")
   async findOne(@Param("id") id: string) {
     const user = await this.usersService.findOneById(id);
@@ -41,13 +39,11 @@ export class UsersController {
     return user;
   }
 
-  @Roles("admin")
   @Patch(":id")
   async update(@Param("id") id: string, @Body() updateUserDTO: UpdateUserDTO) {
     return this.usersService.updateUser(id, updateUserDTO);
   }
 
-  @Roles("admin")
   @Delete(":id")
   async delete(@Param("id") id: string) {
     await this.usersService.deleteUser(id);
